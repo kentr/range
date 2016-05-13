4e83f667c09bb20cacc04d1f16e873867ea425c8
kimallen <kimmarieallen@gmail.com>
2016-05-13 10:37:01 -0700

    Create template file for config info; add config.js to gitignore

---
+++ www/config.template.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    window.conf = function () {
+        return {
+            allowDiscover: false,
+            allowAnalytics: false,
+            analyticsUrl: 'http://www.google-analytics.com/collect',
+            analyticsId: 'GOOGLE_ANALYTICS_ID',
+            mapkey: "BING_MAPKEY",
+            serverUrl: 'SERVER_URL',
+            xAppToken: 'XAP_TOKEN',
+            socrataUrl: 'SOCRATA_URL',
+            libraryUrl: 'LIBRARY_URL',
+            bingScriptSrc: 'http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0',
+            mapUrl: 'http://dev.virtualearth.net/REST/v1/Locations/'
+        };
+    };
+})();
